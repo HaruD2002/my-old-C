12e9bad165f5a8232b02f2024557ba923ab8dc1a
HaruD2002 <77611787+HaruD2002@users.noreply.github.com>
2022-01-26 22:01:15 +0700

    new old c

---
+++ Wk.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int indexof (char *str, int str_length, char *token); 
+// return vi tri xuat hien cua token trong str 
+int sum (char *str);
+
+int main(int argc, char** argv) {
+	char str[1000] = {'a','b','c','d','e'};
+	char token[2] = {'a','b'};
+	char trys[100] = "a123de45e1";
+//	indexof(str,6,token);
+	sum(trys);
+	return 0;
+}
+
+int indexof (char *str,int str_length, char *token){
+	 int len_token = -1;
+	 int i;
+	 
+	 for (i=0;i<=str_length;i++){
+	 	if (token[0] = str[i]){
+	 		len_token = i+1;
+			 return len_token; 
+		 }
+	 }
+	
+	return len_token;
+}
+
+int sum(char *str){
+	int i; 
+	int sum=0;
+	int length;
+	length = strlen(str);
+	int var;
+	var = atoi(str);
+	for (i=0; i<=length;i++){
+		if (isdigit(str)){
+		return sum += atoi(str);
+		
+		}
+	}
+}
